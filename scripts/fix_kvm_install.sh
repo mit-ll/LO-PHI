1ca984ecd2404e7878f75a595225f4ac8ad5f714
Chad Spensky <cspensky@gmail.com>
2016-03-29 20:01:25 -0700

    Create fix_kvm_install.sh

---
+++ scripts/fix_kvm_install.sh
@@ -0,0 +1,25 @@
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+if grep -q "LO-PHI" /etc/apparmor.d/abstractions/libvirt-qemu
+then
+        echo "Exemption already found."
+else
+        echo "Adding AppArmor exception for /tmp for libvmi..."
+        echo "
+  # LO-PHI: Permits libvirt to open a unix socket for memory/disk accesses.
+  /tmp/* rw,
+">> /etc/apparmor.d/abstractions/libvirt-qemu
+fi
+
+# Fix our permissions for libvirt
+sed -i 's/unix_sock_rw_perms = .*/unix_sock_rw_perms = "0777"/' /etc/libvirt/libvirtd.conf
+
+sed -i "s/.*max_clients.*/max_clients = 500/" /etc/libvirt/libvirtd.conf 
+
+
+service libvirt-bin restart
